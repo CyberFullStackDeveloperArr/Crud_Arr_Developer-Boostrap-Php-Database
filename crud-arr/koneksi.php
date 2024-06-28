1876cfd538c2b1a3edd7656ac2cdbdbcec90c24e
arr <127316255+CyberFullStackDeveloperArr@users.noreply.github.com>
2024-06-28 10:44:55 +0700

    koneksi.php

--- crud-arr/koneksi.php
+++ crud-arr/koneksi.php
@@ -9,7 +9,3 @@ $kon = mysqli_connect($host,$user,$password,$db);
 if (!$kon) {
     die("Koneksi Gagal:".mysqli_connect_error());
 }
-// UTAMAKAN IZIN PADA SANG PEMILIK 
-// BYE GHOST CODE NIGHT 
-// ARR DEVELOPER
-// 089520120324
